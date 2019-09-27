da87670493ee78a61e136a965831b32887c26477
Alexander Williams <williams_alex@protonmail.com>
2019-09-27 01:02:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var combined = []
+  var i;
+  for (i=0;i<musicians.length;i++) {
+    combined[i] = musicians[i]+" plays "+instruments[i]
+  }
+  return combined
+}
+
+function johnLennonFacts(factArray) {
+  var newarray = []
+  var i;
+  for (i=0;i<factArray.length;i++) {
+    newarray[i]=factArray[i]+"!!!"
+  }
+  return newarray
+}
